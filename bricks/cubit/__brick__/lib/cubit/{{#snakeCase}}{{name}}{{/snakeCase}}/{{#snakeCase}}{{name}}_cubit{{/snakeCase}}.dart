31d88c3a6021da00d4f158f756e926eb365eba79
Divyanshu Bhargava <divyanshub024@gmail.com>
2022-03-17 19:41:39 +0400

    Add conditional variable

--- bricks/cubit/__brick__/lib/cubit/{{#snakeCase}}{{name}}{{/snakeCase}}/{{#snakeCase}}{{name}}_cubit{{/snakeCase}}.dart
+++ bricks/cubit/__brick__/lib/cubit/{{#snakeCase}}{{name}}{{/snakeCase}}/{{#snakeCase}}{{name}}_cubit{{/snakeCase}}.dart
@@ -1,4 +1,4 @@
-import 'package:equatable/equatable.dart';
+{{#useEquatable}}import 'package:equatable/equatable.dart';{{/useEquatable}}
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part '{{#snakeCase}}{{name}}_state{{/snakeCase}}.dart';
--- bricks/cubit/__brick__/lib/cubit/{{#snakeCase}}{{name}}{{/snakeCase}}/{{#snakeCase}}{{name}}_state{{/snakeCase}}.dart
+++ bricks/cubit/__brick__/lib/cubit/{{#snakeCase}}{{name}}{{/snakeCase}}/{{#snakeCase}}{{name}}_state{{/snakeCase}}.dart
@@ -1,8 +1,10 @@
 part of '{{#snakeCase}}{{name}}_cubit{{/snakeCase}}.dart';
 
-class {{#pascalCase}}{{name}}State{{/pascalCase}} extends Equatable {
+class {{#pascalCase}}{{name}}State{{/pascalCase}} {{#useEquatable}}extends Equatable{{/useEquatable}} {
   const {{#pascalCase}}{{name}}State{{/pascalCase}}();
 
+ {{#useEquatable}}
   @override
   List<Object?> get props => [];
+ {{/useEquatable}}
 }
